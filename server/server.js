c67265cc67812e694c904dd1a22844afc7073d19
Didiloy <didiloy2001@gmail.com>
2022-02-24 16:05:27 +0100

    add : api route handler for  "top250Movies"

--- server/server.js
+++ server/server.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require('cors');
 const port = 4000;
+
+app.use(bodyParser.json());
+app.use(cors());
+
+const top250Movies = require('./api/top250Movies'); //J'utiliser le fichier écrire pour gérer cette requete
+
+app.use('/api/top250Movies', top250Movies); //rediriger les requete de cette url vers le fichier
 
 app.listen(port, () => {
     console.log("listening on port " + port);
