a61f49969c66ae2feeab3d59e8c3687314bda316
Didiloy <didiloy2001@gmail.com>
2022-02-26 23:00:49 +0100

    add: carousel qui fonctionne enfin lets gooooooo

--- client/client/src/views/HomeView.vue
+++ client/client/src/views/HomeView.vue
@@ -10,9 +10,19 @@
           <Sidebar />
       </div>
       <div class="col s10">
-          <div class="carousel">
-            <a class="carousel-item" href="#one!"><img src="https://lorempixel.com/250/250/nature/1"></a>
-            
+          <h2>{{films_populaires}}</h2>
+          <div v-if="computedMostPopularMovies == null">
+            <p>Recherche en cours...</p>
+          </div>
+          <div v-else>
+            <div ref="carousel" class="carousel">
+              <div v-for="movie in mostPopularMovies" v-bind:key="movie.id" class="carousel-item card">
+                <div class="card-image">
+                  <img v-bind:src="movie.image">
+                  <span class="card-title">{{movie.title}}</span>
+                </div>
+              </div>
+            </div>
           </div>
 
           <div class="home">
@@ -34,16 +44,45 @@ import Sidebar from '@/components/Sidebar.vue'
 import M from 'materialize-css'
 import 'materialize-css'
 import 'materialize-css/dist/css/materialize.css'
+const axios = require('axios')
 
 export default {
   name: 'HomeView',
+  data() {
+    return{
+      films_populaires: "Films les plus populaires",
+      mostPopularMovies: null ,
+    }
+  },
+  computed:{
+    computedMostPopularMovies(){
+      return this.mostPopularMovies
+    }
+  },
   components: {
     HelloWorld,
     Navbar,
     Sidebar
   },
+  created(){
+    
+  },
   mounted() {
-    M.AutoInit()
+    M.AutoInit(),
+    this.getMostPopularMovies().then(() => instances = M.Carousel.init(this.$refs.carousel, {numVisible: 5}))
+    console.log(this.$refs.carousel)
+  },
+  methods: {
+    async getMostPopularMovies(){
+      this.mostPopularMovies = {};
+      await axios.get(`http://localhost:4000/api/MostPopularMovies`)
+      .then((response)=> {
+                // console.log(response.data);
+                return (this.mostPopularMovies = response.data);
+            }).catch((error) => {
+                console.log(error);
+            });
+    }
   }
 }
 </script>
