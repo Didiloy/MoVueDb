51acddab69623a3eeb260bf655b9625c17e5ca07
Clement <clement.belet@etu.univ-nantes.fr>
2022-02-26 21:12:56 +0100

    update sidenav

--- client/client/src/components/Sidebar.vue
+++ client/client/src/components/Sidebar.vue
@@ -1,67 +1,13 @@
 <template>
     <div class="background-img">
         <ul id="nav-mobile" class="sidenav sidenav-fixed">
-            
-            
-            
             <li class="no-padding">
             <ul class="collapsible collapsible-accordion">
                 <li class="bold"><a href="about.html" class="collapsible-header waves-effect waves-teal">Abouuuuut</a></li>
                 <li class="bold"><a href="getting-started.html" class="collapsible-header waves-effect waves-teal">Getting Started</a></li>
-                <li class="bold"><a class="collapsible-header waves-effect waves-teal">CSS</a>
-                <div class="collapsible-body">
-                    <ul>
-                    <li><a href="color.html">Color</a></li>
-                    <li><a href="grid.html">Grid</a></li>
-                    <li><a href="helpers.html">Helpers</a></li>
-                    <li><a href="media-css.html">Media</a></li>
-                    <li><a href="pulse.html">Pulse</a></li>
-                    <li><a href="sass.html">Sass</a></li>
-                    <li><a href="shadow.html">Shadow</a></li>
-                    <li><a href="table.html">Table</a></li>
-                    <li><a href="css-transitions.html">Transitions</a></li>
-                    <li><a href="typography.html">Typography</a></li>
-                    </ul>
-                </div>
-                </li>
-                <li class="bold"><a class="collapsible-header waves-effect waves-teal">Components</a>
-                <div class="collapsible-body">
-                    <ul>
-                    <li><a href="badges.html">Badges</a></li>
-                    <li><a href="buttons.html">Buttons</a></li>
-                    <li><a href="breadcrumbs.html">Breadcrumbs</a></li>
-                    <li><a href="cards.html">Cards</a></li>
-                    <li><a href="collections.html">Collections</a></li>
-                    <li><a href="floating-action-button.html">Floating Action Button</a></li>
-                    <li><a href="footer.html">Footer</a></li>
-                    <li><a href="icons.html">Icons</a></li>
-                    <li><a href="navbar.html">Navbar</a></li>
-                    <li><a href="pagination.html">Pagination</a></li>
-                    <li><a href="preloader.html">Preloader</a></li>
-                    </ul>
-                </div>
-                </li>
-                <li class="bold active"><a class="collapsible-header waves-effect waves-teal">JavaScript</a>
-                <div class="collapsible-body">
-                    <ul>
-                    <li><a href="auto-init.html">Auto Init</a></li>
-                    <li><a href="carousel.html">Carousel</a></li>
-                    <li><a href="collapsible.html">Collapsible</a></li>
-                    <li><a href="dropdown.html">Dropdown</a></li>
-                    <li><a href="feature-discovery.html">FeatureDiscovery</a></li>
-                    <li><a href="media.html">Media</a></li>
-                    <li><a href="modals.html">Modals</a></li>
-                    <li><a href="parallax.html">Parallax</a></li>
-                    <li><a href="pushpin.html">Pushpin</a></li>
-                    <li><a href="scrollspy.html">Scrollspy</a></li>
-                    <li class="active"><a href="sidenav.html">Sidenav</a></li>
-                    <li><a href="tabs.html">Tabs</a></li>
-                    <li><a href="toasts.html">Toasts</a></li>
-                    <li><a href="tooltips.html">Tooltips</a></li>
-                    <li><a href="waves.html">Waves</a></li>
-                    </ul>
-                </div>
-                </li>
+                <li class="bold"><a class="collapsible-header waves-effect waves-teal">CSS</a></li>
+                <li class="bold"><a class="collapsible-header waves-effect waves-teal">Components</a></li>
+                <li class="bold"><a class="collapsible-header waves-effect waves-teal">JavaScript</a></li>
                 <li class="bold"><a class="collapsible-header waves-effect waves-teal">Forms</a></li>
                 <li class="bold"><a href="mobile.html" class="collapsible-header waves-effect waves-teal">Mobile</a></li>
                 <li class="bold"><a href="showcase.html" class="collapsible-header waves-effect waves-teal">Showcase</a></li>
@@ -91,8 +37,6 @@ export default {
             this.isPanelOpen = false
         }
     }
-
-
 }
 </script>
 
@@ -108,12 +52,15 @@ export default {
     background-image: url('../assets/background.jpg') !important;
     background-size: cover;
 }
-.sidenav-fixed li{
+.sidenav-fixed .bold{
     background-color: #5F51E5 !important;
+    
 }
-.sidenav-fixed li:hover{
+.sidenav-fixed .bold:hover{
     background-color: #7F74EA !important;
 }
-
+.sidenav-fixed a{
+    color : white !important
+}
 
 </style>
