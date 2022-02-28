448d393fd742726176dc499c25df930699bdcf5a
Didiloy <didiloy2001@gmail.com>
2022-02-28 21:38:20 +0100

    importe vue resource

--- client/client/src/api/api.js
+++ client/client/src/api/api.js
@@ -2,9 +2,11 @@ const KEY = "k_orizm94k";
 const IMDB_BASE_URL = "https://imdb-api.com/fr/API/"
 const axios = require('axios');
 
+
 export async function getCategorie(categorie) {
     return new Promise((resolve, reject) => {
         axios.get(`${IMDB_BASE_URL}/${categorie}/${KEY} +`)
+            // fetch("https://jsonplaceholder.typicode.com/todos/1")
             .then(function(response) {
                 resolve(response.data.items);
             })
