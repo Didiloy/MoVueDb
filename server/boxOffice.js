0deb85782fec7dd95e52e96a78c3f33f4ee836b8
Dylan LOYA <E19L739M@univ-nantes.fr>
2022-03-16 08:54:21 +0100

    server comment

---
+++ server/boxOffice.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+require('dotenv').config();
+
+//get boxOffice
+const URL = `https://imdb-api.com/fr/API/BoxOffice/${process.env.IMDB_API_KEY}`;
+
+router.get('/', (req, res) => {
+    axios.get(URL)
+        .then(function(response) {
+            
+            res.send(response.data.items);
+        })
+        .catch((error) => {
+            res.send(error.message)
+        })
+});
+
+module.exports = router;
